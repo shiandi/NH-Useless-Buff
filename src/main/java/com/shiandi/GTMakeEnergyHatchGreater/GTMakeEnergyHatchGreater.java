9714c511231220289794e35ea41e61784cbe280b
shiandi <1920379084@qq.com>
2025-02-18 18:58:20 +0800

    工业高炉

--- src/main/java/com/shiandi/GTMakeEnergyHatchGreater/GTMakeEnergyHatchGreater.java
+++ src/main/java/com/shiandi/GTMakeEnergyHatchGreater/GTMakeEnergyHatchGreater.java
@@ -10,13 +10,19 @@ import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import cpw.mods.fml.common.event.FMLServerStartingEvent;
 
-@Mod(modid = GTMakeEnergyHatchGreater.MODID, version = Tags.VERSION, name = "GT Make Energy Hatch Greater", acceptedMinecraftVersions = "[1.7.10]")
+@Mod(
+    modid = GTMakeEnergyHatchGreater.MODID,
+    version = Tags.VERSION,
+    name = "GT Make Energy Hatch Greater",
+    acceptedMinecraftVersions = "[1.7.10]")
 public class GTMakeEnergyHatchGreater {
 
     public static final String MODID = "GTMakeEnergyHatchGreater";
     public static final Logger LOG = LogManager.getLogger(MODID);
 
-    @SidedProxy(clientSide = "com.shiandi.GTMakeEnergyHatchGreater.ClientProxy", serverSide = "com.shiandi.GTMakeEnergyHatchGreater.CommonProxy")
+    @SidedProxy(
+        clientSide = "com.shiandi.GTMakeEnergyHatchGreater.ClientProxy",
+        serverSide = "com.shiandi.GTMakeEnergyHatchGreater.CommonProxy")
     public static CommonProxy proxy;
 
     @Mod.EventHandler
