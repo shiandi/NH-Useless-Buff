6d8723f25e07ddb980f24dabc20a7de19d955bae
49979 <sandy1920379084@163.com>
2025-03-03 10:44:37 +0800

    Chaos修改结构检测

--- src/main/java/com/shiandi/NHUselessBuff/mixins/MultiMachines/ChaosMixin.java
+++ src/main/java/com/shiandi/NHUselessBuff/mixins/MultiMachines/ChaosMixin.java
@@ -27,6 +27,6 @@ public abstract class ChaosMixin extends MTEExtendedPowerMultiBlockBase<Chaos> i
     @Inject(method = "checkMachine", at = @At("RETURN"), cancellable = true)
     private void checkMachine(IGregTechTileEntity aBaseMetaTileEntity, ItemStack aStack,
         CallbackInfoReturnable<Boolean> cir) {
-        cir.setReturnValue(checkPiece(STRUCTURE_PIECE_MAIN, 1, 1, 0));
+        cir.setReturnValue(cir.getReturnValue() || checkPiece(STRUCTURE_PIECE_MAIN, 1, 1, 0));
     }
 }
