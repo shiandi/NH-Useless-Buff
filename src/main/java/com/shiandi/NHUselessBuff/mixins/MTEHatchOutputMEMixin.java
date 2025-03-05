e446f1a4b4ba32d89ebbdabc2d5771b1d19721f7
shiandi <1920379084@qq.com>
2025-03-05 15:08:20 +0800

    Mixin

--- src/main/java/com/shiandi/NHUselessBuff/mixins/MTEHatchOutputMEMixin.java
+++ src/main/java/com/shiandi/NHUselessBuff/mixins/MTEHatchOutputMEMixin.java
@@ -1,5 +1,9 @@
 package com.shiandi.NHUselessBuff.mixins;
 
+import java.util.EnumSet;
+
+import net.minecraftforge.common.util.ForgeDirection;
+
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -20,8 +24,13 @@ public abstract class MTEHatchOutputMEMixin extends MTEHatchOutput implements IP
     @Shadow
     boolean additionalConnection;
 
-    @Inject(method = "updateValidGridProxySides", at = @At("HEAD"))
+    @Inject(method = "updateValidGridProxySides", at = @At("HEAD"), cancellable = true)
     private void updateValidGridProxySides(CallbackInfo ci) {
-        this.additionalConnection = true;
+        if (!additionalConnection) {
+            getProxy().setValidSides(EnumSet.complementOf(EnumSet.of(ForgeDirection.UNKNOWN)));
+        } else {
+            getProxy().setValidSides(EnumSet.of(getBaseMetaTileEntity().getFrontFacing()));
+        }
+        ci.cancel();
     }
 }
